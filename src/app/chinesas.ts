1456da721573c75f418d083a2c6cdebbc090facc
João Pedro <you@example.com>
2019-12-02 22:50:05 -0200

    atualizado

---
+++ src/app/chinesas.ts
@@ -0,0 +1,23 @@
+export const chinesas = [
+  {
+    name: 'Carne chop suey',
+    price: 20.99,
+    description: 'Nesta receita possui couve-flor, brócolis, acelga, cenoura, broto, presunto, carne de gado',
+    imagem: "assets/temakiHot.png"
+  },
+
+  {
+    name: 'teste',
+    price: 25.99,
+    description: 'Nes presunto, carne de gado',
+    imagem:"assets/temakiSalmao.png"
+
+  },
+  {
+    name: 'Teste 3',
+    price: 12.99,
+    description: 'Teste teste',
+    imagem:"assets/yakisoba.png"
+
+  }
+];
